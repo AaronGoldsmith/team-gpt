963d9b34f71416aff161516fe2dd448972ffdec0
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2023-05-04 08:21:49 -0700

    modify personality class to accept loaded traits

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from prompt_generator.PromptGenerator import PromptGenerator
+import json
+# from prompt_generator.PromptGenerator import PromptGenerator
 from prompt_generator.personalities.Personality import Personality
 if __name__ == '__main__':
     # professions = prompt_generator.generate_professions(['Doctor', 'Boxer', 'Astronaut'])
     # generator = PromptGenerator()
-    # description = generator.get_trait_description('Partier')
-    # personality = Personality(extraversion=1,agreeableness=9,boring=4)
-    # personality2 = Personality(extraversion=9,agreeableness=2,selfish=4)
-    personality3 = Personality(extraversion=9,agreeableness=2,funny=4)
-    personality2 = Personality(extraversion=9,agreeableness=2,funny=4,custom_traits_path="generated/custom_traits.json")
+    # description = generator.get_trait_description('Shy')
+    personality1 = Personality(extraversion=9,agreeableness=2,serious=4)
+    personality2 = Personality(extraversion=9,agreeableness=2,motivated=5, suspicious=1,custom_traits_path="generated/traits.json")
+    Personality.save_personality(personality2,'p1.json')
 
-
+    personality3 = Personality.load_personality('p1.json')
+    print(personality2.traits == personality3.traits)
+    print(personality2.custom_traits == personality3.custom_traits)
+    print(json.dumps(personality2.__dict__,indent=2))
+    print(json.dumps(personality3.__dict__,indent=2))
--- prompt_generator/personalities/Personality.py
+++ prompt_generator/personalities/Personality.py
@@ -15,22 +15,25 @@ class Personality:
         """
         try:
             with open(cls.custom_traits_path, "r") as file:
-                cls.CUSTOM_TRAITS = json.load(file)
+                cls.custom_traits = json.load(file)
         except FileNotFoundError:
             pass
 
     # required traits first, and custom/optional at the end
-    def __init__(self, extraversion: int, agreeableness: int, custom_traits_path=None, **kwargs):
+    def __init__(self, extraversion: int, agreeableness: int, custom_traits_path=None, saved_traits={}, **kwargs):
         self.traits = {
             "extraversion": extraversion,
             "agreeableness": agreeableness
         }
-        self.custom_traits_path = custom_traits_path or Personality.custom_traits_path
-        self.DEFAULT_TRAITS = DEFAULT_TRAITS
-        self.CUSTOM_TRAITS = {}
+        self.custom_traits_path = (custom_traits_path or Personality.custom_traits_path)
+        self.custom_traits = saved_traits
         self.load_custom_traits()
         self.add_custom_traits(kwargs)
         self.traits.update(kwargs)
+
+        self.trait_defs = {k: v for k, v in DEFAULT_TRAITS.items() if k in self.traits}
+
+
 
         
     def add_custom_traits(self, traits):
@@ -38,9 +41,9 @@ class Personality:
         Takes a list of traits, and assigns descriptions from GPT
         """
         for trait in traits:
-            if trait not in self.DEFAULT_TRAITS and trait not in self.CUSTOM_TRAITS:
+            if trait not in DEFAULT_TRAITS and trait not in self.custom_traits:
                 description = Personality.prompt_generator.get_trait_description(trait)
-                self.CUSTOM_TRAITS[trait] = json.loads(description)[trait]
+                self.custom_traits[trait] = json.loads(description)[trait]
                 self.save_custom_traits()
 
     def save_custom_traits(self):
@@ -53,9 +56,40 @@ class Personality:
         except FileNotFoundError:
             existing_traits = {}
         with open(self.custom_traits_path, "w") as file:
-            custom_traits = {**existing_traits, **self.CUSTOM_TRAITS}
+            custom_traits = {**existing_traits, **self.custom_traits}
             json.dump(custom_traits, file, indent=2)
-            
+    
+
+    def save_personality(personality, filename):
+        """
+        Save personality to a file
+        """
+        # traits_with_desc = {}
+        # for trait, value in personality.traits.items():
+        #     if trait in DEFAULT_TRAITS:
+        #         desc = DEFAULT_TRAITS[trait]
+        #     else:
+        #         desc = ""
+        #     traits_with_desc[trait] = {"value": value, "description": desc}
+
+        # Save personality data to file
+        # with open(filename, 'w') as f:
+            # json.dump(traits_with_desc, f, indent=2)
+        with open(filename, 'w') as f:
+            json.dump(personality.__dict__, f)
+
+    def load_personality(filename):
+        """
+        Load personality from a file
+        """
+        with open(filename, 'r') as f:
+            personality_data = json.load(f)
+        # print(personality_data)
+        # print()
+        d = personality_data['custom_traits']
+        print(d)
+
+        return Personality( **personality_data['traits'], saved_traits=personality_data["custom_traits"])
 
 # Load custom traits at module level
 Personality.load_custom_traits()
